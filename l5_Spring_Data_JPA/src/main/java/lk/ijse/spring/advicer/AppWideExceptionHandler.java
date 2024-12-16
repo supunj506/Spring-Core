1cfcc36e6e489dbcc6f9c3b016af80fae9e0179a
supunj506 <supunj506@gmail.com>
2024-12-16 22:05:54 +0530

    update

---
+++ l5_Spring_Data_JPA/src/main/java/lk/ijse/spring/advicer/AppWideExceptionHandler.java
@@ -0,0 +1,10 @@
+/*
+ * @author : xCODE
+ * Project : SpringFramework
+ * Date    : 12/15/2024 (Sunday)
+ * Time    : 8:32 PM
+ * For GDSE course of IJSE institute.
+ */
+ 
+package lk.ijse.spring.advicer;public class AppWideExceptionHandler {
+}
